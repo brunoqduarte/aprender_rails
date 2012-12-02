99d47a31bb21e4e4555326e49a2cb29715238ae2
Bruno Queiroz <brunoqduarte@gmail.com>
2012-12-02 20:26:44 -0300

    alteracoes

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,15 @@
+class SignupController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    
+    if @user.save
+      redirect_to login_path, :notice => t("flash.signup.create.notice")
+    else
+      render :new
+    end
+  end
+end
---
+++ spec/integration/signup_spec.rb
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+
+require "spec_helper"
+
+describe "Signup" do
+  context "with valid data" do
+    before do
+      visit root_path
+      click_link "Quero me cadastrar"
+      fill_in "Seu nome", :with => "John Doe"
+      fill_in "Seu e-mail", :with => "john@example.org"
+      fill_in "Sua senha", :with => "test"
+      fill_in "Confirme sua senha", :with => "test"
+      click_button "Quero me cadastrar"
+    end
+
+    it "redirects to the login page" do
+      expect(current_path).to eql(login_path)
+    end
+
+    it "display succes message" do
+      expect(page).to have_content("Usuário cadastrado com sucesso!")
+    end
+
+  end
+  
+  context "with invalid data" do
+    before do
+      visit root_path
+      click_link "Quero me cadastrar"
+      click_button "Quero me cadastrar"
+    end
+    
+    it "display error messages" do
+      expect(page).to have_content("Verifique o formulário antes de continuar:")
+    end
+    
+  end
+
+end
